8ac1929f8f56eaa4aa07214e1c4f5edc99e37699
RuhaimHakeem <ruhaim860@gmail.com>
2021-12-31 23:12:29 +0530

    first commit

---
+++ src/signupform/signupform.js
@@ -0,0 +1,21 @@
+const SignUpForm = () => {
+    return (  
+    <form> 
+        <div className="form-inner">
+            <h2> Sign up </h2>
+            <p>Sign up To Continue With The Process</p>
+
+            <div className="form-group">
+                <input type="text" class = "icon" placeholder='Enter Phone Number ' />
+                <input type="text"placeholder='First name ' />
+                <input type="text"placeholder='Last name ' />
+                <input id="partitioned" type="text" maxlength="5" />
+
+            </div>    
+            <input type='submit' value='Continue'/>   
+            
+        </div>
+    </form> );
+}
+ 
+export default SignUpForm;
