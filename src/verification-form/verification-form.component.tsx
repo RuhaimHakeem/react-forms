8653c6e20529a0fc096150c31dd814a2b0ce7ba4
RuhaimHakeem <ruhaim860@gmail.com>
2022-01-04 18:00:34 +0530

    verification-form

--- src/verification-form/verification-form.component.tsx
+++ src/verification-form/verification-form.component.tsx
@@ -3,11 +3,38 @@ import styled from "styled-components";
 import { Heading, SubHeading } from "../text";
 import { Button } from "../button";
 import { space, SpaceProps } from "styled-system";
+import { useRef } from "react";
 
 export const Verification: React.FC = () => {
   const submitHandler: React.FormEventHandler = (e) => {
     alert("Registered");
     e.preventDefault();
+  };
+
+  const inputRef1 = React.useRef<HTMLInputElement>(null);
+  const inputRef2 = React.useRef<HTMLInputElement>(null);
+  const inputRef3 = React.useRef<HTMLInputElement>(null);
+  const inputRef4 = React.useRef<HTMLInputElement>(null);
+  const inputRef5 = React.useRef<HTMLInputElement>(null);
+  const inputRef6 = React.useRef<HTMLInputElement>(null);
+
+  const focus1 = () => {
+    inputRef1?.current?.focus();
+  };
+  const focus2 = () => {
+    inputRef2?.current?.focus();
+  };
+  const focus3 = () => {
+    inputRef3?.current?.focus();
+  };
+  const focus4 = () => {
+    inputRef4?.current?.focus();
+  };
+  const focus5 = () => {
+    inputRef5?.current?.focus();
+  };
+  const focus6 = () => {
+    inputRef6?.current?.focus();
   };
 
   const SFormGroup = styled.div`
@@ -36,17 +63,72 @@ export const Verification: React.FC = () => {
     ${space};
   `;
 
+  const handleBackSpace = (e: any) => {
+    if (e.keyCode === 8 && e.target.value("")) {
+      inputRef5?.current?.focus();
+    }
+  };
+
   return (
     <SForm onSubmit={submitHandler}>
       <Heading> OTP Verification </Heading>
       <SubHeading>This is to verify your phone number</SubHeading>
       <SFormGroup>
-        <SInput mt="16px" type="text" id="first" required maxLength={1} />
-        <SInput mt="16px" type="text" id="second" required maxLength={1} />
-        <SInput mt="16px" type="text" id="third" required maxLength={1} />
-        <SInput mt="16px" type="text" id="fourth" required maxLength={1} />
-        <SInput mt="16px" type="text" id="fifth" required maxLength={1} />
-        <SInput mt="16px" type="text" id="sixth" required maxLength={1} />
+        <SInput
+          ref={inputRef1}
+          mt="16px"
+          type="text"
+          id="first"
+          required
+          onChange={focus2}
+          maxLength={1}
+          autoFocus
+        />
+        <SInput
+          ref={inputRef2}
+          mt="16px"
+          type="text"
+          id="second"
+          required
+          maxLength={1}
+          onChange={focus3}
+        />
+        <SInput
+          ref={inputRef3}
+          mt="16px"
+          type="text"
+          id="third"
+          required
+          maxLength={1}
+          onChange={focus4}
+        />
+        <SInput
+          ref={inputRef4}
+          mt="16px"
+          type="text"
+          id="fourth"
+          required
+          maxLength={1}
+          onChange={focus5}
+        />
+        <SInput
+          ref={inputRef5}
+          mt="16px"
+          type="text"
+          id="fifth"
+          required
+          maxLength={1}
+          onChange={focus6}
+        />
+        <SInput
+          ref={inputRef6}
+          mt="16px"
+          type="text"
+          id="sixth"
+          required
+          maxLength={1}
+          onKeyDown={handleBackSpace}
+        />
       </SFormGroup>
       <SubHeading> Enter 6-digit code</SubHeading>
       <Button mt="24px">Verify</Button>
